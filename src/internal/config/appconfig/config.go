20037ada2783c6e5aba9c7cbee076f591d70a79f
ridhoestu.b <ridhoestu.b@gmail.com>
2026-07-05 17:28:42 +0700

    config: add frontend url and google client id settings

--- src/internal/config/appconfig/config.go
+++ src/internal/config/appconfig/config.go
@@ -23,6 +23,7 @@ type Config struct {
 	Redis     redis.RedisOptions            `yaml:"redis"`
 	Business  business.BusinessConfig       `yaml:"business"`
 	SMTP      smtp.SMTPConfig               `yaml:"smtp"`
+	OAuth     OAuthConfig                   `yaml:"oauth"`
 }
 
 type AppConfig struct {
@@ -30,10 +31,15 @@ type AppConfig struct {
 	Version         string        `yaml:"version" env:"APP_VERSION" env-default:"1.0.0"`
 	Port            int           `yaml:"port" env:"APP_PORT" env-default:"8080"`
 	Environment     string        `yaml:"environment" env:"APP_ENVIRONMENT" env-default:"development"`
+	FrontendBaseURL string        `yaml:"frontend_base_url" env:"FRONTEND_BASE_URL" env-default:""`
 	WriteTimeout    time.Duration `yaml:"write_timeout" env:"APP_WRITE_TIMEOUT" env-default:"15s"`
 	ReadTimeout     time.Duration `yaml:"read_timeout" env:"APP_READ_TIMEOUT" env-default:"15s"`
 	IdleTimeout     time.Duration `yaml:"idle_timeout" env:"APP_IDLE_TIMEOUT" env-default:"60s"`
 	ShutdownTimeout time.Duration `yaml:"shutdown_timeout" env:"APP_SHUTDOWN_TIMEOUT" env-default:"30s"`
+}
+
+type OAuthConfig struct {
+	GoogleClientID string `yaml:"google_client_id" env:"GOOGLE_CLIENT_ID"`
 }
 
 func LoadConfig() (*Config, error) {
