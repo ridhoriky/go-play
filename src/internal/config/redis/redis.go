5e116b1300b46f86dc936a09247ff7a6a384b781
ridhoestu.b <ridhoestu.b@gmail.com>
2026-07-28 10:08:48 +0700

    fix: uppdate config redis

--- src/internal/config/redis/redis.go
+++ src/internal/config/redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"context"
+	"crypto/tls"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -10,18 +11,22 @@ import (
 
 type RedisOptions struct {
 	Addr     string        `yaml:"addr" env:"REDIS_ADDR" env-default:"localhost:6379"`
+	Username string        `yaml:"username" env:"REDIS_USERNAME" env-default:""`
 	Password string        `yaml:"password" env:"REDIS_PASSWORD" env-default:""`
 	DB       int           `yaml:"db" env:"REDIS_DB" env-default:"0"`
 	PoolSize int           `yaml:"pool_size" env:"REDIS_POOL_SIZE" env-default:"10"`
 	Timeout  time.Duration `yaml:"timeout" env:"REDIS_TIMEOUT" env-default:"5s"`
+	TLS      bool          `yaml:"tls" env:"REDIS_TLS" env-default:"false"`
 }
 
 func InitRedis(log *zerolog.Logger, cfg *RedisOptions) (*redis.Client, error) {
 	client := redis.NewClient(&redis.Options{
-		Addr:     cfg.Addr,
-		Password: cfg.Password,
-		DB:       cfg.DB,
-		PoolSize: cfg.PoolSize,
+		Addr:      cfg.Addr,
+		Username:  cfg.Username,
+		Password:  cfg.Password,
+		DB:        cfg.DB,
+		PoolSize:  cfg.PoolSize,
+		TLSConfig: &tls.Config{},
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
